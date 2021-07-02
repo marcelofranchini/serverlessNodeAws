5373f64e8d8d9e0f68294d5e6b96edddad262de5
Marcelo Franchini <marcelo_franchini@hotmail.com>
2021-07-01 22:00:05 -0300

    desafio serverless

---
+++ src/app.js
@@ -0,0 +1,131 @@
+var AWS = require('aws-sdk');
+var uuid = require('uuid');
+AWS.config.update({ region: 'us-east-1' });
+var docClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+
+exports.lambdaHandler = async (event, context, callback) => {
+    let body;
+
+    if (event.httpMethod == 'POST') {
+        body = await createEmployee(JSON.parse(event.body));
+    }
+    if (event.httpMethod == 'GET') {
+        body = await getEmployee(event.pathParameters.id);
+    }
+
+    if (event.httpMethod == 'PUT') {
+        body = await updateEmployee(event.pathParameters.id, JSON.parse(event.body));
+    }
+
+    if (event.httpMethod == 'DELETE') {
+        body = await deleteEmployee(event.pathParameters.id);
+    }
+
+    const response = {
+        statusCode: 200,
+        body: JSON.parse(body)
+    }
+
+    return response;
+};
+
+async function getEmployee(id) {
+
+    var params = {
+        TableName: 'Employee',
+        Key: { 'id': id }
+    };
+
+    let body;
+
+    try {
+        const data = await docClient.get(params).promise();
+
+        body = await data.Item
+
+    } catch (err) {
+        body = err;
+    }
+
+    return body
+}
+
+async function createEmployee(event) {
+
+    var params = {
+        TableName: 'Employee',
+        Item: {
+            'id': uuid.v4(),
+            'name': event.name,
+            'position': event.position,
+            'age': event.age
+        }
+    };
+
+    let body;
+    try {
+        const data = await docClient.put(params).promise();
+        body = data.Item
+
+    } catch (err) {
+        console.log(err);
+        body = err;
+    }
+
+    const response = {
+        statusCode: 200,
+        body: JSON.parse(body)
+    }
+    return response.body
+}
+
+async function updateEmployee(id, event) {
+
+    var params = {
+        TableName: 'Employee',
+        Key: { 'id': id },
+        UpdateExpression: "set name = :name, position = :position, age = :age",
+        ExpressionAttributeValues: {
+            ':name': event.name,
+            ':position': event.position,
+            ':age': event.age
+        },
+        ReturnValues: "UPDATED_NEW"
+    };
+
+    let body;
+
+    try {
+        const data = await docClient.update(params).promise();
+
+        body = data.Item
+
+    } catch (err) {
+        console.log(err);
+        body = err;
+    }
+
+    return body
+}
+
+async function deleteEmployee(id) {
+
+    var params = {
+        TableName: 'Employee',
+        Key: { 'id': id }
+    };
+
+    let body;
+
+    try {
+        const data = await docClient.delete(params).promise();
+
+        body = data.Item
+
+    } catch (err) {
+        console.log(err);
+        body = err;
+    }
+
+    return body
+}
---
+++ y
@@ -0,0 +1,10 @@
+version = 0.1
+[y]
+[y.deploy]
+[y.deploy.parameters]
+stack_name = "marcelo-stefanini"
+s3_bucket = "aws-sam-cli-managed-default-samclisourcebucket-x5xgmjnejocq"
+s3_prefix = "marcelo-stefanini"
+region = "us-east-1"
+confirm_changeset = true
+capabilities = "CAPABILITY_IAM"
